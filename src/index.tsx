fc7c03463c00375336d47bac35718550fa02515f
ErinSY <seoyul0203@gmail.com>
2022-12-02 23:16:42 +0900

    ADD: Styled component

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import styled, { ThemeProvider } from 'styled-components';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ThemeProvider>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
