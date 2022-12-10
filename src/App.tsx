2ffbf7f2ee16cde0234717b2aeebe7c2ee8c7680
ErinSY <seoyul0203@gmail.com>
2022-12-10 20:05:16 +0900

    ADD: Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import Circle from './Circle';
+import React, { useState } from 'react';
 
 function App() {
+  const [username, setuseName] = useState('');
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    console.log(event.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = event;
+    setuseName(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('hello', username);
+  };
   return (
     <div>
-      <Circle bgColor='tomato' borderColor='black' text='나는 지정함'/>
-      <Circle bgColor='teal' />
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          placeholder='username'
+          value={username}
+          onChange={onChange}
+        />
+        <button>submit</button>
+      </form>
     </div>
   );
 }
--- src/dist/App.js
+++ src/dist/App.js
@@ -1,9 +1,20 @@
 "use strict";
 exports.__esModule = true;
-var Circle_1 = require("./Circle");
+var react_1 = require("react");
 function App() {
-    return (React.createElement("div", null,
-        React.createElement(Circle_1["default"], { bgColor: 'tomato', borderColor: 'black', text: '\uB098\uB294 \uC9C0\uC815\uD568' }),
-        React.createElement(Circle_1["default"], { bgColor: 'teal' })));
+    var _a = react_1.useState(''), username = _a[0], setuseName = _a[1];
+    var onChange = function (event) {
+        console.log(event.currentTarget.value);
+        var value = event.currentTarget.value;
+        setuseName(value);
+    };
+    var onSubmit = function (event) {
+        event.preventDefault();
+        console.log('hello', username);
+    };
+    return (react_1["default"].createElement("div", null,
+        react_1["default"].createElement("form", { onSubmit: onSubmit },
+            react_1["default"].createElement("input", { type: 'text', placeholder: 'username', value: username, onChange: onChange }),
+            react_1["default"].createElement("button", null, "submit"))));
 }
 exports["default"] = App;
